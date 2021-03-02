d7176691d5bfce9e18c3eb357fad4b96632244f3
ozumin <mizuo.n@gmail.com>
2021-03-02 22:37:19 +0900

    Day 29: Project 5, Challenge 1

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -111,6 +111,12 @@ class ViewController: UITableViewController {
     }
 
     func isReal(word: String) -> Bool {
+        if word.count < 3 {
+            return false
+        }
+        if word.first == title!.first {
+            return false
+        }
         let checker = UITextChecker()
         let range = NSRange(location: 0, length: word.utf16.count)
         let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
