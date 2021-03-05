7b942e40729bc35c66afc003434243830f6d7f2c
ozumin <mizuo.n@gmail.com>
2021-03-05 23:43:38 +0900

    Day 32: Challenge

---
+++ Day32/Day31/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Day31
+//
+//  Created by Mizuo Nagayama on 2021/03/05.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var itemList: [String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addItemToList))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(refreshList))
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemList.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Item", for: indexPath)
+        cell.textLabel?.text = itemList[indexPath.row]
+        return cell
+    }
+    
+    @objc func addItemToList() {
+        let ac = UIAlertController(title: "Add item", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) { [weak self, weak ac] action in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
+    
+    func submit(_ answer: String) {
+        if isReal(answer) {
+            itemList.insert(answer, at: 0)
+            let indexPath = IndexPath(row: 0, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        } else {
+            let ac = UIAlertController(title: "Item is not real word", message: nil, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    func isReal(_ answer: String) -> Bool {
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: answer.utf16.count)
+        let misspelledRange = checker.rangeOfMisspelledWord(in: answer, range: range, startingAt: 0, wrap: false, language: "en")
+        return misspelledRange.location == NSNotFound
+    }
+    
+    @objc func refreshList() {
+        itemList = []
+        tableView.reloadData()
+    }
+}
+
