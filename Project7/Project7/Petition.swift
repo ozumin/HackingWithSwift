330e3d5b46269f5b73cb8a322beab757cfa32589
ozumin <mizuo.n@gmail.com>
2021-03-07 17:59:57 +0900

    Day 33: Project 7 parsing json

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  petition.swift
+//  Project7
+//
+//  Created by Mizuo Nagayama on 2021/03/07.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
