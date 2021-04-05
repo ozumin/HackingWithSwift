9804bbc0b4ba35c06d93e27803d2dd9a0905b401
ozumin <mizuo.n@gmail.com>
2021-04-05 21:48:37 +0900

    Day 54: Project 13 Challenge 1

--- Project13/Project13/ViewController.swift
+++ Project13/Project13/ViewController.swift
@@ -75,7 +75,12 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func save(_ sender: Any) {
-        guard let image = imageView.image else { return }
+        guard let image = imageView.image else {
+            let ac = UIAlertController(title: "No image", message: nil, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+            return
+        }
         UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
     
