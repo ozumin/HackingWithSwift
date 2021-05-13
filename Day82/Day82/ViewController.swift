999ea3d71ddacf770be73d70aebf6d32e24b7e32
ozumin <mizuo.n@gmail.com>
2021-05-13 23:08:07 +0900

    Day 82: Challenge 2

--- Day82/Day82/ViewController.swift
+++ Day82/Day82/ViewController.swift
@@ -14,6 +14,10 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         self.label.bounceOut(duration: 5)
+
+        5.times {
+            print("Hello!")
+        }
     }
 
 
@@ -23,6 +27,14 @@ private extension UIView {
     func bounceOut(duration: Double) {
         UIView.animate(withDuration: duration) {
             self.transform = CGAffineTransform(scaleX: 0.0001, y: 0.0001)
+        }
+    }
+}
+
+private extension Int {
+    func times(_ closure: () -> Void) {
+        for _ in 0..<self {
+            closure()
         }
     }
 }
