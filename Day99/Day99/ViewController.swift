35a98d92bec5e21193da6e9f47c73d1c99d90d7d
ozumin <mizuo.n@gmail.com>
2021-06-06 23:12:29 +0900

    Day 99: init

---
+++ Day99/Day99/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Day99
+//
+//  Created by Mizuo Nagayama on 2021/06/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.view.backgroundColor = .systemBackground
+    }
+
+
+}
+
